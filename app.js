045557ba72e07491f718d8360da801cc51b1263b
Deepakshi <deepakshisud@gmail.com>
2021-02-01 14:55:50 +0530

    Boilerplate started

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const Safetypin = require('./models/safetypin');
 const methodOverride = require('method-override');
 
@@ -19,7 +20,7 @@ db.once("open", () => {
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname, '/views'));
-
+app.engine('ejs',ejsMate);
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
