53b11f4a900cd0b67d2eb5573efdf65bfb9a818a
Deepakshi <deepakshisud@gmail.com>
2021-02-13 12:04:59 +0530

    Restructuring routes

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError')
 const ejsMate = require('ejs-mate');
 const {safetypinSchema, reviewSchema} = require('./schemas.js');
-const Safetypin = require('./models/safetypin');
 const methodOverride = require('method-override');
-const Review = require('./models/review');
 const safetypins = require('./routes/safetypins');
+const reviews = require('./routes/reviews');
 
 
 mongoose.connect('mongodb://localhost:27017/safety-pin', {
@@ -31,50 +29,14 @@ app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
 
-const validateSafetypin = (req,res,next) => {
-    const {error} = safetypinSchema.validate(req.body);
-    if(error) {
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg , 400);
-    } else {
-        next();
-    }
-}
-
-const validateReview = (req,res, next) => {
-    const {error} = reviewSchema.validate(req.body);
-    if(error) {
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg , 400);
-    } else {
-        next();
-    }
-}
-
-app.use('/safetypins', safetypins)
+app.use('/safetypins', safetypins);
+app.use('/safetypins/:id/reviews', reviews);
 
 
 app.get('/', (req, res) => {
     res.render('home');
 })
 
-
-
-app.post('/safetypins/:id/reviews', validateReview ,catchAsync(async(req, res) => {
-    const safetypin = await Safetypin.findById(req.params.id);
-    const review = new Review(req.body.review);
-    safetypin.reviews.push(review);
-    await review.save();
-    await safetypin.save();
-    res.redirect(`/safetypins/${safetypin._id}`);
-}))
-
-app.delete('/safetypins/:id/reviews/:reviewId', catchAsync(async(req, res)=> {
-    const {id, reviewId} = req.params;
-    await Safetypin.findByIdAndUpdate(id, {$pull: {review: reviewId}})
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/safetypins/${id}`)
-}))
 
 app.all('*',(req,res,next) => {
     next(new ExpressError('Page Not found',404));
---
+++ routes/reviews.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router({mergeParams: true});
+const catchAsync = require('../utils/catchAsync');
+const ExpressError = require('../utils/ExpressError')
+const Safetypin = require('../models/safetypin');
+const Review = require('../models/review');
+const {reviewSchema} = require('../schemas.js');
+
+const validateReview = (req,res, next) => {
+    const {error} = reviewSchema.validate(req.body);
+    if(error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg , 400);
+    } else {
+        next();
+    }
+}
+
+router.post('/', validateReview ,catchAsync(async(req, res) => {
+    const safetypin = await Safetypin.findById(req.params.id);
+    const review = new Review(req.body.review);
+    safetypin.reviews.push(review);
+    await review.save();
+    await safetypin.save();
+    res.redirect(`/safetypins/${safetypin._id}`);
+}))
+
+router.delete('/:reviewId', catchAsync(async(req, res)=> {
+    const {id, reviewId} = req.params;
+    await Safetypin.findByIdAndUpdate(id, {$pull: {review: reviewId}})
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/safetypins/${id}`)
+}))
+
+module.exports = router;
