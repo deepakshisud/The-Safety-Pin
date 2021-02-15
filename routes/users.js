8aaa50115791e22fb01d4204b80a8768c54ee544
Deepakshi <deepakshisud@gmail.com>
2021-02-15 14:14:40 +0530

    Added authentication

--- routes/users.js
+++ routes/users.js
@@ -16,16 +16,21 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) => {
     req.flash('success', "Welcome back");
-    res.redirect('/safetypins');
+    const redirectUrl = req.session.returnTo || '/safetypins';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
 })
 
-router.post('/register', catchAsync(async(req, res) => {
+router.post('/register', catchAsync(async(req, res, next) => {
     try{
         const {email, username, password} = req.body;
         const user = new User({email, username});
         const registeredUser = await User.register(user, password);
-        req.flash('success', "Successfully registered");
-        res.redirect('/safetypins');
+        req.login(registeredUser, err=> {
+            if(err) return next(err);
+            req.flash('success', "Successfully registered");
+            res.redirect('/safetypins');
+        })
     } catch(e) {
         req.flash('error',e.message);
         res.redirect('register');
