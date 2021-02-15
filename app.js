a6d8273a07fe1077e63cfe1b5a975fa13ea48f42
Deepakshi <deepakshisud@gmail.com>
2021-02-15 12:58:56 +0530

    Added authentication

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
-
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 
 const safetypins = require('./routes/safetypins');
@@ -47,6 +49,9 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
 
 
 app.use((req, res, next) => {
