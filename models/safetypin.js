64da8e015bdf9f77a88ef36db3737cc40be86ea8
Deepakshi <deepakshisud@gmail.com>
2021-02-19 22:32:07 +0530

    Image upload and update added

--- models/safetypin.js
+++ models/safetypin.js
@@ -3,15 +3,19 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url : String,
+    filename: String
+})
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
+
 const SafetypinSchema = new Schema( {
     location: String,
-    images : [
-        {
-            url : String,
-            filename: String
-
-        }
-    ],
+    images : [ImageSchema],
     safety_index: Number,
     description: String,
     address: String,
