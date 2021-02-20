ef3ecfbdcd8c2ab056bc6af3e2aadb5d566470bb
Deepakshi <deepakshisud@gmail.com>
2021-02-20 11:07:34 +0530

    Map added to showpage

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -1,8 +1,12 @@
-
+const pins = JSON.parse(pin);
 mapboxgl.accessToken = mapToken;
+
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    center: pins.geometry.coordinates, // starting position [lng, lat]
+    zoom: 12 // starting zoom
 });
+new mapboxgl.Marker()
+    .setLngLat(pins.geometry.coordinates)
+    .addTo(map)
